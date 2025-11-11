e83f17a38963617bd36eb19a2c9bbc11de284f31
koushikdev22 <koushik.journey.mondal@gmail.com>
2025-11-11 16:17:24 +0530

    new

--- src/migrations/1762858028661-CreateUserTable.ts
+++ src/migrations/1762858028661-CreateUserTable.ts
@@ -1,12 +1,11 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class CreateUserTable1762783661390 implements MigrationInterface {
-    name = 'CreateUserTable1762783661390'
+export class CreateUserTable1762858028661 implements MigrationInterface {
+    name = 'CreateUserTable1762858028661'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`DROP INDEX \`IDX_fe0bb3f6520ee0469504521e71\` ON \`users\``);
         await queryRunner.query(`DROP INDEX \`IDX_97672ac88f789774dd47f7c8be\` ON \`users\``);
-        await queryRunner.query(`ALTER TABLE \`users\` ADD \`role_id\` bigint NOT NULL`);
         await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`organization_name\` \`organization_name\` varchar(255) NULL`);
         await queryRunner.query(`ALTER TABLE \`users\` ADD UNIQUE INDEX \`IDX_fe0bb3f6520ee0469504521e71\` (\`username\`)`);
         await queryRunner.query(`ALTER TABLE \`users\` ADD UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` (\`email\`)`);
@@ -15,9 +14,11 @@ export class CreateUserTable1762783661390 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`otp\` \`otp\` varchar(255) NULL`);
         await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`otp_expired_at\` \`otp_expired_at\` varchar(255) NULL`);
         await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`refresh_token\` \`refresh_token\` text NULL`);
+        await queryRunner.query(`ALTER TABLE \`users\` ADD CONSTRAINT \`FK_a2cecd1a3531c0b041e29ba46e1\` FOREIGN KEY (\`role_id\`) REFERENCES \`roles\`(\`id\`) ON DELETE NO ACTION ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`users\` DROP FOREIGN KEY \`FK_a2cecd1a3531c0b041e29ba46e1\``);
         await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`refresh_token\` \`refresh_token\` text NULL DEFAULT 'NULL'`);
         await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`otp_expired_at\` \`otp_expired_at\` varchar(255) NULL DEFAULT 'NULL'`);
         await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`otp\` \`otp\` varchar(255) NULL DEFAULT 'NULL'`);
@@ -26,7 +27,6 @@ export class CreateUserTable1762783661390 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE \`users\` DROP INDEX \`IDX_97672ac88f789774dd47f7c8be\``);
         await queryRunner.query(`ALTER TABLE \`users\` DROP INDEX \`IDX_fe0bb3f6520ee0469504521e71\``);
         await queryRunner.query(`ALTER TABLE \`users\` CHANGE \`organization_name\` \`organization_name\` varchar(255) NULL DEFAULT 'NULL'`);
-        await queryRunner.query(`ALTER TABLE \`users\` DROP COLUMN \`role_id\``);
         await queryRunner.query(`CREATE UNIQUE INDEX \`IDX_97672ac88f789774dd47f7c8be\` ON \`users\` (\`email\`)`);
         await queryRunner.query(`CREATE UNIQUE INDEX \`IDX_fe0bb3f6520ee0469504521e71\` ON \`users\` (\`username\`)`);
     }
